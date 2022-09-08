a8c08d7904686f75a454e7066b3533d6efbf44e4
kuryart <kuryart1@gmail.com>
2022-09-08 16:47:12 -0300

    feat: adiciona funções save e destroy ao repository de aluno

--- app/Repository/AlunoRepository.php
+++ app/Repository/AlunoRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repository\Eloquent;
+namespace App\Repository;
 
 use App\Models\Aluno;
 use App\Repository\Interface\AlunoRepositoryInterface;
@@ -10,21 +10,33 @@ use Illuminate\Support\Collection;
 class AlunoRepository extends BaseRepository implements AlunoRepositoryInterface
 {
 
-   /**
-    * AlunoRepository constructor.
-    *
-    * @param Aluno $model
-    */
-   public function __construct(Aluno $model)
-   {
-       parent::__construct($model);
-   }
+    /**
+     * AlunoRepository constructor.
+     *
+     * @param Aluno $model
+     */
+    public function __construct(Aluno $model)
+    {
+        parent::__construct($model);
+    }
 
-   /**
-    * @return Collection
-    */
-   public function all(): Collection
-   {
-       return $this->model->all();    
-   }
+    /**
+     * @return Collection
+     */
+    public function all(): Collection
+    {
+        return $this->model->all();
+    }
+
+    public function save(Aluno $aluno)
+    {
+        $aluno->save();
+        return $aluno;
+    }
+
+    public function destroy(Aluno $aluno)
+    {
+        $aluno->delete();
+        return $aluno;
+    }
 }
--- app/Repository/Interface/AlunoRepositoryInterface.php
+++ app/Repository/Interface/AlunoRepositoryInterface.php
@@ -2,9 +2,12 @@
 
 namespace App\Repository\Interface;
 
+use App\Models\Aluno;
 use Illuminate\Support\Collection;
 
 interface AlunoRepositoryInterface
 {
    public function all(): Collection;
+   public function save(Aluno $aluno);
+   public function destroy(Aluno $aluno);
 }
