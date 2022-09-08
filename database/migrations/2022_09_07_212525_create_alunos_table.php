e9941e150156be915ec60c3c1afe949ea5b25672
kuryart <kuryart1@gmail.com>
2022-09-08 16:46:35 -0300

    fix: adiciona limite de tamanho à coluna CPF na migration de Alunos

--- database/migrations/2022_09_07_212525_create_alunos_table.php
+++ database/migrations/2022_09_07_212525_create_alunos_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('nome');
             $table->string('email');
             $table->string('ra')->unique();
-            $table->string('cpf');
+            $table->string('cpf', 11);
             $table->timestamps();
         });
     }
